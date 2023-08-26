20175b25a4991c7a772d4cbaf158cbef419f1e25
kritik <kritikverma2002@gmail.com>
2023-08-26 19:21:11 +0530

    Footer added
    bug fix
    
    bug fix
    
    test

--- src/components/shared/Footer/Footer.js
+++ src/components/shared/Footer/Footer.js
@@ -2,77 +2,77 @@ import React from "react";
 import "./Footer.css";
 function Footer() {
   return (
-    <footer class="footer">
-      <div class="container">
-        <div class="row">
-          <div class="footer-col">
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="footer-col">
             <h4>company</h4>
             <ul>
               <li>
-                <a href="#">about us</a>
+                <a href="/#">about us</a>
               </li>
               <li>
-                <a href="#">our services</a>
+                <a href="/#">our services</a>
               </li>
               <li>
-                <a href="#">privacy policy</a>
+                <a href="/#">privacy policy</a>
               </li>
               <li>
-                <a href="#">affiliate program</a>
+                <a href="/#">affiliate program</a>
               </li>
             </ul>
           </div>
-          <div class="footer-col">
+          <div className="footer-col">
             <h4>get help</h4>
             <ul>
               <li>
-                <a href="#">FAQ</a>
+                <a href="/#">FAQ</a>
               </li>
               <li>
-                <a href="#">shipping</a>
+                <a href="/#">shipping</a>
               </li>
               <li>
-                <a href="#">returns</a>
+                <a href="/#">returns</a>
               </li>
               <li>
-                <a href="#">order status</a>
+                <a href="/#">order status</a>
               </li>
               <li>
-                <a href="#">payment options</a>
+                <a href="/#">payment options</a>
               </li>
             </ul>
           </div>
-          <div class="footer-col">
+          <div className="footer-col">
             <h4>online shop</h4>
             <ul>
               <li>
-                <a href="#">watch</a>
+                <a href="/#">watch</a>
               </li>
               <li>
-                <a href="#">bag</a>
+                <a href="/#">bag</a>
               </li>
               <li>
-                <a href="#">shoes</a>
+                <a href="/#">shoes</a>
               </li>
               <li>
-                <a href="#">dress</a>
+                <a href="/#">dress</a>
               </li>
             </ul>
           </div>
-          <div class="footer-col">
+          <div className="footer-col">
             <h4>follow us</h4>
-            <div class="social-links">
-              <a href="#">
-                <i class="fab fa-facebook-f"></i>
+            <div className="social-links">
+              <a href="/#">
+                <i className="fab fa-facebook-f"></i>
               </a>
-              <a href="#">
-                <i class="fab fa-twitter"></i>
+              <a href="/#">
+                <i className="fab fa-twitter"></i>
               </a>
-              <a href="#">
-                <i class="fab fa-instagram"></i>
+              <a href="/#">
+                <i className="fab fa-instagram"></i>
               </a>
-              <a href="#">
-                <i class="fab fa-linkedin-in"></i>
+              <a href="/#">
+                <i className="fab fa-linkedin-in"></i>
               </a>
             </div>
           </div>
